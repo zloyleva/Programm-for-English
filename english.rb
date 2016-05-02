d14a82ed3d9fb08d82c470d5d3ad8674c772219c
Levchenko Oleg <levchenko.oleh.ua@gmail.com>
2016-05-02 20:55:53 +0300

    Add new words and rewrite ruby file

--- english.rb
+++ english.rb
@@ -6,3 +6,13 @@ while (line = input.gets)
 	words_array << line.strip.split(",")
 end
 input.close
+
+puts "Количество слов в словаре : #{words_array.size}"
+answer_true = 0.00
+answer_all = 0.00
+
+while true
+	number_ru = rand(0..words_array.size)
+	print "Введите слово '#{words_array[number_ru][1]}' на английском или нажмите Enter для выхода: "
+	user_word = gets.strip
+end
